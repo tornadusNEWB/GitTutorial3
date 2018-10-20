77a76b2b6ba847581bcd2c49f3668b51604f4497
tornadusNEWB <nowaczek5@o2.pl>
2018-10-20 13:51:32 +0200

    changes in main

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -3,6 +3,6 @@ package com.sda.git;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("kurs gita 3");
+        System.out.println("kurs gita 333");
     }
 }
